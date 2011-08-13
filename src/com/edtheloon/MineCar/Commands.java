3f55d1f9ee6c4d7debb1a05e15794ab6452db94d
Adrian Reid <devilred101@gmail.com>
2011-08-13 08:11:44 +0100

    Preparations

--- src/com/edtheloon/MineCar/Commands.java
+++ src/com/edtheloon/MineCar/Commands.java
@@ -1,5 +1,24 @@
 package com.edtheloon.MineCar;
 
-public class Commands {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 
+public class Commands implements CommandExecutor {
+	
+	// Class variables
+	public static Plugin plugin;
+
+	// CONSTRUCTOR
+	public Commands (Plugin plug) {
+		this.plugin = plug;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		return false;
+		
+	}
+	
 }
