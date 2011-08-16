4057b6fa496168ca351fe056b791f2cf691f52c3
Adrian Reid <devilred101@gmail.com>
2011-08-16 08:12:56 +0100

    Added new class

---
+++ src/com/edtheloon/MineCar/MCPlayerListener.java
@@ -0,0 +1,15 @@
+package com.edtheloon.MineCar;
+
+import org.bukkit.event.player.PlayerListener;
+
+public class MCPlayerListener extends PlayerListener {
+	
+	// Class Variables
+	private MCMain plugin;
+	
+	// CONSTRUCTOR
+	public MCPlayerListener(MCMain plug) {
+		this.plugin = plug;
+	}
+
+}
