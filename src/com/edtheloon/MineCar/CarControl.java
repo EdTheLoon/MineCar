3ab13eaaa2adf2ab38ce80de4bf6579e7327a76a
Adrian Reid <devilred101@gmail.com>
2011-08-16 06:06:57 +0100

    Experimental code.

--- src/com/edtheloon/MineCar/CarControl.java
+++ src/com/edtheloon/MineCar/CarControl.java
@@ -9,26 +9,28 @@ public class CarControl {
 	public static void moveForward(SpoutPlayer player) {
 		// TODO: ADD MOVEMENT CODE
 		Vehicle cart = player.getVehicle();
-		Vector vel = new Vector();
-		vel.setX(0.5);
-		vel.setY(0);
-		vel.setY(0);
+		Vector vel = cart.getVelocity();
+		
+		vel.setX(1);
 		cart.setVelocity(vel);
 	}
 	
 	public static void moveBackward(SpoutPlayer player) {
 		// TODO: ADD MOVEMENT CODE
 		Vehicle cart = player.getVehicle();
-		Vector vel = new Vector();
-		vel.setX(-0.5);
-		vel.setY(0);
-		vel.setY(0);
+		Vector vel = cart.getVelocity();
+		
+		vel.setX(-1);
 		cart.setVelocity(vel);
 	}
 	
 	public static void turn(SpoutPlayer player, String direction) {
 		// TODO: ADD TURNING CODE
 		Vehicle cart = player.getVehicle();
+		Vector vel = cart.getVelocity();
+		
+		if (direction == "left") vel.setY(0.5); else vel.setY(-0.5);
+		cart.setVelocity(vel);
 	}
 
 }
