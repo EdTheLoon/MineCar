488ad58919d67980ff73ccc4d84ee56b9a2eb1db
Adrian Reid <devilred101@gmail.com>
2011-08-16 05:54:17 +0100

    Added some movement code - going to test

--- src/com/edtheloon/MineCar/CarControl.java
+++ src/com/edtheloon/MineCar/CarControl.java
@@ -1,19 +1,34 @@
 package com.edtheloon.MineCar;
 
+import org.bukkit.entity.Vehicle;
+import org.bukkit.util.Vector;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class CarControl {
 	
 	public static void moveForward(SpoutPlayer player) {
 		// TODO: ADD MOVEMENT CODE
+		Vehicle cart = player.getVehicle();
+		Vector vel = new Vector();
+		vel.setX(0.5);
+		vel.setY(0);
+		vel.setY(0);
+		cart.setVelocity(vel);
 	}
 	
 	public static void moveBackward(SpoutPlayer player) {
 		// TODO: ADD MOVEMENT CODE
+		Vehicle cart = player.getVehicle();
+		Vector vel = new Vector();
+		vel.setX(-0.5);
+		vel.setY(0);
+		vel.setY(0);
+		cart.setVelocity(vel);
 	}
 	
 	public static void turn(SpoutPlayer player, String direction) {
 		// TODO: ADD TURNING CODE
+		Vehicle cart = player.getVehicle();
 	}
 
 }
