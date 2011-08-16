b9c318df248bc85361fd750e94afa43e92bf5f69
Adrian Reid <devilred101@gmail.com>
2011-08-16 02:16:01 +0100

    Added new class

---
+++ src/com/edtheloon/MineCar/MCEntityListener.java
@@ -0,0 +1,15 @@
+package com.edtheloon.MineCar;
+
+import org.bukkit.event.entity.EntityListener;
+
+public class MCEntityListener extends EntityListener {
+	
+	// Class Variables
+	private MCMain plugin;
+	
+	// CONSTRUCTOR
+	public MCEntityListener(MCMain plug) {
+		this.plugin = plug;
+	}
+
+}
