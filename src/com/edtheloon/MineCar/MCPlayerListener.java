ab22d3f78db61a62b7351275f50442f05c8bc78e
Adrian Reid <devilred101@gmail.com>
2011-08-16 08:19:59 +0100

    Added some code in preparation for turning using the mouse!

--- src/com/edtheloon/MineCar/MCPlayerListener.java
+++ src/com/edtheloon/MineCar/MCPlayerListener.java
@@ -1,5 +1,8 @@
 package com.edtheloon.MineCar;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerAnimationEvent;
+import org.bukkit.event.player.PlayerAnimationType;
 import org.bukkit.event.player.PlayerListener;
 
 public class MCPlayerListener extends PlayerListener {
@@ -10,6 +13,22 @@ public class MCPlayerListener extends PlayerListener {
 	// CONSTRUCTOR
 	public MCPlayerListener(MCMain plug) {
 		this.plugin = plug;
+	}
+	
+	public void onPlayerAnimation(PlayerAnimationEvent event) {
+		// Variables
+		Player player = event.getPlayer();
+		
+		// Check to see if the player is inside a vehicle
+		if (player.isInsideVehicle()) {
+			// Check that the vehicle the player is in is their MineCar
+			if (player.getVehicle().getEntityId() == plugin.mineCars.get(player.getName())) {
+				// Now check that the player was swinging their arm
+				if (event.getAnimationType() == PlayerAnimationType.ARM_SWING) {
+					// TODO: DO SOME MINECAR TURNING CODE
+				}
+			}
+		}
 	}
 
 }
