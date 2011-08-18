375fa1ecd098565c09844b1c196186cf22e2a533
Lathanael <101@abwesend.de>
2011-08-18 16:16:14 +0200

    Added code for loading and saveing cars Needs testing.

--- src/com/edtheloon/MineCar/MCMain.java
+++ src/com/edtheloon/MineCar/MCMain.java
@@ -10,51 +10,51 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCMain extends JavaPlugin {
-	
+
 	// PERMISSION STRINGS
 	public static final String PERMISSION_CONTROL = "MineCar.control";
 	public static final String PERMISSION_CREATE = "MineCar.create";
 	public static final String PERMISSION_REMOVE = "MineCar.remove";
-	
+
 	// Class Variables
 	public PluginManager pluginManager;
 	public final Logger log = Logger.getLogger("Minecraft");
 	public HashMap<String,Integer> mineCars;
 	public File carsFile = new File("plugins/MineCars/cars.txt");
-	
+
 	public void onEnable() {
-		
+
 		// Output to console that we're loading the list of MineCars from a file and then load the list
 		log.info("[MineCar] Loading list of MineCars...");
-		mineCars = Functions.loadCars();
-		
+		mineCars = Functions.loadCars(carsFile);
+
 		// Get the server's plugin manager so we can register commands and events
-		pluginManager = getServer().getPluginManager(); 
-		
+		pluginManager = getServer().getPluginManager();
+
 		// Register some events
 		pluginManager.registerEvent(Type.VEHICLE_DESTROY, new MCVehicleListener(this), Priority.Normal, this);
 		pluginManager.registerEvent(Type.PLAYER_ANIMATION, new MCPlayerListener(this), Priority.Normal, this);
-		
+
 		// Register INPUT LISTENING events
 		pluginManager.registerEvent(Type.CUSTOM_EVENT, new MCInputListener(this), Priority.Normal, this);
-		
+
 		// Set Commands to be our commandExecutor
 		getCommand("minecar").setExecutor(new Commands(this));
-		
+
 		// Output to server console that the plugin is enabled
 		log.info("[MineCar] Version " + this.getDescription().getVersion() + " enabled");
-		
+
 	}
-	
+
 	public void onDisable() {
-		
+
 		// Output to server console that we're saving the cars list and then load the list
 		log.info("[MineCar] Saving list of cars...");
-		Functions.saveCars(mineCars);
-		
+		Functions.saveCars(mineCars, carsFile);
+
 		// Output to server console that the plugin is disabled
 		log.info("[MineCar] Version " + this.getDescription().getVersion() + " disabled");
-		
+
 	}
 
 }
